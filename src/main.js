ba05f6d4afa41af47a9b118f2bb262813313d5d1
BIGSTONE\user <louie38628050@gmai.com>
2026-04-16 17:04:55 +0800

    建立 Vite 初始專案

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div class="container">
+    <h1>掛機彈彈彈 Web 版</h1>
+    <p>Vite 專案已成功啟動</p>
+    <button id="startBtn">開始遊戲</button>
+  </div>
+`
---
+++ vite.config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/Nkust-114-2-me/',
+})
